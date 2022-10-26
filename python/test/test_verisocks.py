0e59c03406cb0b8d29d22ecd5bc08b7d8213e741
jchabloz <jeremie.chabloz@a3.epfl.ch>
2022-10-26 07:21:33 +0200

    Properly use pytest.fixture

--- python/test/test_verisocks.py
+++ python/test/test_verisocks.py
@@ -16,21 +16,15 @@ sim_info_product = "Icarus Verilog"
 sim_info_version = "11.0 (stable)"
 
 
-# @pytest.fixture
-# def pop():
-#     _pop = setup_iverilog("test_0.v")
-#     yield _pop
-#     _pop.wait(timeout=120)
-
-
-# @pytest.fixture
-# def vs(pop):
-#     assert pop.poll() is None
-#     _vs = Verisocks(HOST, PORT)
-#     _vs.connect()
-#     yield _vs
-#     _vs.finish()def test_run_for_time():
-#     _vs.close()
+@pytest.fixture
+def vs():
+    pop = setup_iverilog("test_0.v")
+    _vs = Verisocks(HOST, PORT)
+    _vs.connect()
+    yield _vs
+    _vs.finish()
+    _vs.close()
+    pop.wait(timeout=120)
 
 
 def get_abspath(relpath):
@@ -69,277 +63,211 @@ def setup_iverilog(src_file):
     return pop
 
 
-def test_connect():
+def test_connect(vs):
     """Tests simple connection to Icarus simulator with Verisocks server"""
-    pop = setup_iverilog("test_0.v")
-    assert pop.poll() is None
-    with Verisocks(HOST, PORT) as vs:
-        assert vs._connected
-        answer = vs.finish()
-        assert answer["type"] == "ack"
-    retcode = pop.wait(timeout=120)
-    assert retcode == 0
+    assert vs._connected
 
-    # Verify error case - Here Icarus is not running anymore
+
+def test_connect_error():
+    """Tests trying to connect to a non-running server"""
     with pytest.raises(ConnectionRefusedError):
-        with Verisocks(HOST, PORT) as vs:
-            pass
+        vs = Verisocks(HOST, PORT)
+        vs.connect()
+    vs.close()
 
 
-def test_get_sim_info():
-    """Tests Verisocks get(sel=sim_info) function"""
-    pop = setup_iverilog("test_0.v")
-    with Verisocks(HOST, PORT) as vs:
-        answer = vs.get(sel="sim_info")
-        assert answer["type"] == "result"
-        assert answer["product"] == sim_info_product
-        assert answer["version"] == sim_info_version
-        answer = vs.finish()
-        assert answer["type"] == "ack"
-    retcode = pop.wait(timeout=120)
-    assert retcode == 0
+def test_get_sim_info(vs):
+    answer = vs.get(sel="sim_info")
+    assert answer["type"] == "result"
+    assert answer["product"] == sim_info_product
+    assert answer["version"] == sim_info_version
 
 
-def test_get_sim_time():
+def test_get_sim_time(vs):
     """Tests Verisocks get(sel=sim_time) function"""
-    # Setup
-    pop = setup_iverilog("test_0.v")
-    with Verisocks(HOST, PORT) as vs:
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] == 0.0
 
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] == 0.0
-        answer = vs.run(cb="until_time", time=101.3, time_unit="us")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] == 101.3e-6
+    answer = vs.run(cb="until_time", time=101.3, time_unit="us")
+    assert answer["type"] == "ack"
 
-        # Teardown
-        answer = vs.finish()
-        assert answer["type"] == "ack"
-    retcode = pop.wait(timeout=120)
-    assert retcode == 0
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] == 101.3e-6
 
 
-def test_get_value():
+def test_get_value(vs):
     """Tests Verisocks get(sel="value") function"""
 
-    # Setup
-    pop = setup_iverilog("test_0.v")
-    with Verisocks(HOST, PORT) as vs:
+    # Get an integer parameter value
+    answer = vs.get(sel="value", path="main.num_port")
+    assert answer["type"] == "result"
+    assert answer["value"] == 5100
 
-        # Get an integer parameter value
-        answer = vs.get(sel="value", path="main.num_port")
-        assert answer["type"] == "result"
-        assert answer["value"] == 5100
+    # Get a real parameter value
+    answer = vs.get(sel="value", path="main.fclk")
+    assert answer["type"] == "result"
+    assert answer["value"] == 1.01
 
-        # Get a real parameter value
-        answer = vs.get(sel="value", path="main.fclk")
-        assert answer["type"] == "result"
-        assert answer["value"] == 1.01
+    # Get a reg value
+    answer = vs.run(cb="for_time", time=100, time_unit="us")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="value", path="main.clk")
+    assert answer["type"] == "result"
+    assert answer["value"] == 1
 
-        # Get a reg value
-        answer = vs.run(cb="for_time", time=100, time_unit="us")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="value", path="main.clk")
-        assert answer["type"] == "result"
-        assert answer["value"] == 1
+    # Get a reg[] value
+    answer = vs.get(sel="value", path="main.count")
+    assert answer["type"] == "result"
+    assert answer["value"] == 101
 
-        # Get a reg[] value
-        answer = vs.get(sel="value", path="main.count")
-        assert answer["type"] == "result"
-        assert answer["value"] == 101
+    # Get a memory array value
+    answer = vs.get(sel="value", path="main.count_memory")
+    assert answer["type"] == "result"
+    assert answer["value"] == [
+        96, 97, 98, 99, 100, 85, 86, 87,
+        88, 89, 90, 91, 92, 93, 94, 95]
 
-        # Get a memory array value
-        answer = vs.get(sel="value", path="main.count_memory")
-        assert answer["type"] == "result"
-        assert answer["value"] == [
-            96, 97, 98, 99, 100, 85, 86, 87,
-            88, 89, 90, 91, 92, 93, 94, 95]
-
-        # Error case: wrong path
-        with pytest.raises(VerisocksError):
-            answer = vs.get(sel="value", path="wrong_path")
-            assert answer["type"] == "error"
-
-        # Teardown
-        answer = vs.finish()
-        assert answer["type"] == "ack"
-    retcode = pop.wait(timeout=120)
-    assert retcode == 0
+    # Error case: wrong path
+    with pytest.raises(VerisocksError):
+        answer = vs.get(sel="value", path="wrong_path")
+        assert answer["type"] == "error"
 
 
-def test_run_for_time():
+def test_run_for_time(vs):
     """Tests Verisocks run(cb="for_time") function"""
-    # Setup
-    pop = setup_iverilog("test_0.v")
-    with Verisocks(HOST, PORT) as vs:
 
-        # Get initial time
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        prev_sim_time = answer["time"]
+    # Get initial time
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    prev_sim_time = answer["time"]
 
-        # For time in ps
-        answer = vs.run(cb="for_time", time=6, time_unit="ps")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] - prev_sim_time == pytest.approx(6e-12)
-        prev_sim_time = answer["time"]
+    # For time in ps
+    answer = vs.run(cb="for_time", time=6, time_unit="ps")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] - prev_sim_time == pytest.approx(6e-12)
+    prev_sim_time = answer["time"]
 
-        # For time in ns
-        answer = vs.run(cb="for_time", time=7, time_unit="ns")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] - prev_sim_time == pytest.approx(7e-9)
-        prev_sim_time = answer["time"]
+    # For time in ns
+    answer = vs.run(cb="for_time", time=7, time_unit="ns")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] - prev_sim_time == pytest.approx(7e-9)
+    prev_sim_time = answer["time"]
 
-        # For time in us
-        answer = vs.run(cb="for_time", time=8, time_unit="us")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] - prev_sim_time == pytest.approx(8e-6)
-        prev_sim_time = answer["time"]
+    # For time in us
+    answer = vs.run(cb="for_time", time=8, time_unit="us")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] - prev_sim_time == pytest.approx(8e-6)
+    prev_sim_time = answer["time"]
 
-        # For time in ms
-        answer = vs.run(cb="for_time", time=0.23, time_unit="ms")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] - prev_sim_time == pytest.approx(0.23e-3)
-        prev_sim_time = answer["time"]
+    # For time in ms
+    answer = vs.run(cb="for_time", time=0.23, time_unit="ms")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] - prev_sim_time == pytest.approx(0.23e-3)
+    prev_sim_time = answer["time"]
 
-        # For time in s
-        answer = vs.run(cb="for_time", time=0.00017, time_unit="s")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] - prev_sim_time == pytest.approx(0.17e-3)
-        prev_sim_time = answer["time"]
-
-        # Teardown
-        answer = vs.finish()
-        assert answer["type"] == "ack"
-    retcode = pop.wait(timeout=120)
-    assert retcode == 0
+    # For time in s
+    answer = vs.run(cb="for_time", time=0.00017, time_unit="s")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] - prev_sim_time == pytest.approx(0.17e-3)
+    prev_sim_time = answer["time"]
 
 
-def test_run_until_time():
+def test_run_until_time(vs):
     """Tests Verisocks run(cb="until_time") function"""
-    # Setup
-    pop = setup_iverilog("test_0.v")
-    with Verisocks(HOST, PORT) as vs:
 
-        # Until time in ps
-        answer = vs.run(cb="until_time", time=174, time_unit="ps")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] == pytest.approx(174e-12)
+    # Until time in ps
+    answer = vs.run(cb="until_time", time=174, time_unit="ps")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] == pytest.approx(174e-12)
 
-        # Until time in ns
-        answer = vs.run(cb="until_time", time=215.4, time_unit="ns")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] == pytest.approx(215.4e-9)
+    # Until time in ns
+    answer = vs.run(cb="until_time", time=215.4, time_unit="ns")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] == pytest.approx(215.4e-9)
 
-        # Until time in us
-        answer = vs.run(cb="until_time", time=1.593, time_unit="us")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] == pytest.approx(1.593e-6)
+    # Until time in us
+    answer = vs.run(cb="until_time", time=1.593, time_unit="us")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] == pytest.approx(1.593e-6)
 
-        # Until time in ms
-        answer = vs.run(cb="until_time", time=0.456, time_unit="ms")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] == pytest.approx(0.456e-3)
+    # Until time in ms
+    answer = vs.run(cb="until_time", time=0.456, time_unit="ms")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] == pytest.approx(0.456e-3)
 
-        # Until time in s
-        answer = vs.run(cb="until_time", time=0.6e-3, time_unit="s")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="sim_time")
-        assert answer["type"] == "result"
-        assert answer["time"] == pytest.approx(0.6e-3)
+    # Until time in s
+    answer = vs.run(cb="until_time", time=0.6e-3, time_unit="s")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="sim_time")
+    assert answer["type"] == "result"
+    assert answer["time"] == pytest.approx(0.6e-3)
 
-        # Error: until time in the past
-        with pytest.raises(VerisocksError):
-            answer = vs.run(cb="until_time", time=0.4e-3, time_unit="s")
-            assert answer["type"] == "error"
-
-        # Teardown
-        answer = vs.finish()
-        assert answer["type"] == "ack"
-    retcode = pop.wait(timeout=120)
-    assert retcode == 0
+    # Error: until time in the past
+    with pytest.raises(VerisocksError):
+        answer = vs.run(cb="until_time", time=0.4e-3, time_unit="s")
+        assert answer["type"] == "error"
 
 
-def test_run_until_change():
+def test_run_until_change(vs):
     """Tests Verisocks run(cb="until_change") function"""
-    # Setup
-    pop = setup_iverilog("test_0.v")
-    with Verisocks(HOST, PORT) as vs:
 
-        answer = vs.run(cb="until_change", path="main.count", value=137)
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="value", path="main.count")
-        assert answer["type"] == "result"
-        assert answer["value"] == 137
+    answer = vs.run(cb="until_change", path="main.count", value=137)
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="value", path="main.count")
+    assert answer["type"] == "result"
+    assert answer["value"] == 137
 
-        answer = vs.run(cb="until_change", path="main.counter_end")
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="value", path="main.count")
-        assert answer["type"] == "result"
-        assert answer["value"] == 255
-
-        # Teardown
-        answer = vs.finish()
-        assert answer["type"] == "ack"
-    retcode = pop.wait(timeout=120)
-    assert retcode == 0
+    answer = vs.run(cb="until_change", path="main.counter_end")
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="value", path="main.count")
+    assert answer["type"] == "result"
+    assert answer["value"] == 255
 
 
-def test_set():
+def test_set(vs):
     """Tests Verisocks set() function"""
-    # Setup
-    pop = setup_iverilog("test_0.v")
-    with Verisocks(HOST, PORT) as vs:
+    # Set a reg
+    answer = vs.run(cb="until_time", time=10, time_unit="us")
+    assert answer["type"] == "ack"
+    answer = vs.set(path="main.count", value=125)
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="value", path="main.count")
+    assert answer["type"] == "result"
+    assert answer["value"] == 125
 
-        # Set a reg
-        answer = vs.run(cb="until_time", time=10, time_unit="us")
-        assert answer["type"] == "ack"
-        answer = vs.set(path="main.count", value=125)
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="value", path="main.count")
-        assert answer["type"] == "result"
-        assert answer["value"] == 125
+    # Set a memory array
+    answer = vs.set(path="main.count_memory", value=list(range(16)))
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="value", path="main.count_memory")
+    assert answer["type"] == "result"
+    assert answer["value"] == list(range(16))
 
-        # Set a memory array
-        answer = vs.set(path="main.count_memory", value=list(range(16)))
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="value", path="main.count_memory")
-        assert answer["type"] == "result"
-        assert answer["value"] == list(range(16))
+    answer = vs.set(path="main.count_memory[6]", value=37)
+    assert answer["type"] == "ack"
+    answer = vs.get(sel="value", path="main.count_memory")
+    assert answer["type"] == "result"
+    assert answer["value"][6] == 37
 
-        answer = vs.set(path="main.count_memory[6]", value=37)
-        assert answer["type"] == "ack"
-        answer = vs.get(sel="value", path="main.count_memory")
-        assert answer["type"] == "result"
-        assert answer["value"][6] == 37
-
-        # Set an event
-        answer = vs.set(path="main.counter_end")
-        assert answer["type"] == "ack"
-
-        # Teardown
-        answer = vs.finish()
-        assert answer["type"] == "ack"
-    retcode = pop.wait(timeout=120)
-    assert retcode == 0
+    # Set an event
+    answer = vs.set(path="main.counter_end")
+    assert answer["type"] == "ack"
