93fde862246baeebd9305dfa607e4c73324bbe85
jchabloz <jeremie.chabloz@a3.epfl.ch>
2022-12-09 09:50:45 +0100

    Check support for __VA_OPT__

--- include/vs_logging.h
+++ include/vs_logging.h
@@ -31,6 +31,14 @@
 #define VS_VPI_LOG_LEVEL LEVEL_DEBUG
 #endif
 
+/* Macros to verify if C preprocessor supports __VA_OPT__ */
+#define PP_THIRD_ARG(a,b,c,...) c
+#define VA_OPT_SUPPORTED_I(...) PP_THIRD_ARG(__VA_OPT__(,),true,false,)
+#define VA_OPT_SUPPORTED VA_OPT_SUPPORTED_I(?)
+
+/******************************************************************************/
+#ifdef VA_OPT_SUPPORTED
+/******************************************************************************/
 /* Generic */
 #define VS_LOG_MODNAME(LEVEL, MODNAME, fmt, ...) \
     fprintf(stderr, LEVEL " [" MODNAME "]: " \
@@ -138,5 +146,120 @@
 #else
 #define vs_vpi_log_critical(fmt, ...)
 #endif
+
+/******************************************************************************/
+#else  // __VA_OPT__ not supported
+/******************************************************************************/
+/* Generic */
+#define VS_LOG_MODNAME(LEVEL, MODNAME, fmt, ...) \
+    fprintf(stderr, LEVEL " [" MODNAME "]: " \
+    fmt "\n", ##__VA_ARGS__)
+
+#define VS_VPI_LOG(LEVEL, fmt, ...) \
+    vpi_printf(LEVEL " [Verisocks]: " fmt "\n", ##__VA_ARGS__)
+
+#define VS_LOG(LEVEL, fmt, ...) \
+    fprintf(stderr, LEVEL ": " \
+    fmt "\n", ##__VA_ARGS__)
+
+/* Debug-level macros */
+#if (VS_LOG_LEVEL <= LEVEL_DEBUG)
+#define vs_log_mod_debug(modname, fmt, ...) \
+    VS_LOG_MODNAME("DEBUG", modname, fmt, ##__VA_ARGS__)
+#define vs_log_debug(fmt, ...) \
+    VS_LOG("DEBUG", fmt, ##__VA_ARGS__)
+#else
+#define vs_log_mod_debug(modname, fmt, ...)
+#define vs_log_debug(fmt, ...)
+#endif
+
+#if (VS_VPI_LOG_LEVEL <= LEVEL_DEBUG)
+#define vs_vpi_log_debug(fmt, ...) \
+    VS_VPI_LOG("DEBUG", fmt, ##__VA_ARGS__)
+#else
+#define vs_vpi_log_debug(fmt, ...)
+#endif
+
+/* Info-level macros */
+#if (VS_LOG_LEVEL <= LEVEL_INFO)
+#define vs_log_mod_info(modname, fmt, ...) \
+    VS_LOG_MODNAME("INFO", modname, fmt, ##__VA_ARGS__)
+#define vs_log_info(fmt, ...) \
+    VS_LOG("INFO", fmt, ##__VA_ARGS__)
+#else
+#define vs_log_mod_info(modname, fmt, ...)
+#define vs_log_info(fmt, ...)
+#endif
+
+#if (VS_VPI_LOG_LEVEL <= LEVEL_INFO)
+#define vs_vpi_log_info(fmt, ...) \
+    VS_VPI_LOG("INFO", fmt, ##__VA_ARGS__)
+#else
+#define vs_vpi_log_info(fmt, ...)
+#endif
+
+/* Warning-level macros */
+#if (VS_LOG_LEVEL <= LEVEL_WARNING)
+#define vs_log_mod_warning(modname, fmt, ...) \
+    VS_LOG_MODNAME("WARNING", modname, fmt, ##__VA_ARGS__)
+#define vs_log_warning(fmt, ...) \
+    VS_LOG("WARNING", fmt, ##__VA_ARGS__)
+#else
+#define vs_log_mod_warning(modname, fmt, ...)
+#define vs_log_warning(fmt, ...)
+#endif
+
+#if (VS_VPI_LOG_LEVEL <= LEVEL_WARNING)
+#define vs_vpi_log_warning(fmt, ...) \
+    VS_VPI_LOG("WARNING", fmt, ##__VA_ARGS__)
+#else
+#define vs_vpi_log_warning(fmt, ...)
+#endif
+
+/* Error-level macros */
+#if (VS_LOG_LEVEL <= LEVEL_ERROR)
+#define vs_log_mod_error(modname, fmt, ...) \
+    VS_LOG_MODNAME("ERROR", modname, fmt, ##__VA_ARGS__)
+#define vs_log_error(fmt, ...) \
+    VS_LOG("ERROR", fmt, ##__VA_ARGS__)
+#define vs_log_mod_perror(modname, val) \
+    perror("ERROR [" modname "]: " val)
+#define vs_log_perror(val) \
+    perror("ERROR: " val)
+#else
+#define vs_log_mod_error(modname, fmt, ...)
+#define vs_log_error(fmt, ...)
+#define vs_log_mod_perror(modname, val)
+#define vs_log_perror(val)
+#endif
+
+#if (VS_VPI_LOG_LEVEL <= LEVEL_ERROR)
+#define vs_vpi_log_error(fmt, ...) \
+    VS_VPI_LOG("ERROR", fmt, ##__VA_ARGS__)
+#else
+#define vs_vpi_log_error(fmt, ...)
+#endif
+
+/* Critical-level macros */
+#if (VS_LOG_LEVEL <= LEVEL_CRITICAL)
+#define vs_log_mod_critical(modname, fmt, ...) \
+    VS_LOG_MODNAME("CRITICAL", modname, fmt, ##__VA_ARGS__)
+#define vs_log_critical(fmt, ...) \
+    VS_LOG("CRITICAL", fmt, ##__VA_ARGS__)
+#else
+#define vs_log_mod_critical(modname, fmt, ...)
+#define vs_log_critical(fmt, ...)
+#endif
+
+#if (VS_VPI_LOG_LEVEL <= LEVEL_CRITICAL)
+#define vs_vpi_log_critical(fmt, ...) \
+    VS_VPI_LOG("CRITICAL", fmt, ##__VA_ARGS__)
+#else
+#define vs_vpi_log_critical(fmt, ...)
+#endif
+
+
+#endif
+
 
 #endif //VS_LOGGING_H
