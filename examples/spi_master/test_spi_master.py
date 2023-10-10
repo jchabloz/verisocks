473caf21e3d7ce0a489fbe1345bd1db2a4311561
jchabloz <jeremie.chabloz@a3.epfl.ch>
2023-10-10 14:49:30 +0200

    Replaced PIPE with DEVNULL, added randomization, added __main__ execution

--- examples/spi_master/test_spi_master.py
+++ examples/spi_master/test_spi_master.py
@@ -6,6 +6,7 @@ import shutil
 import logging
 import pytest
 import socket
+import random
 
 
 def find_free_port():
@@ -65,8 +66,8 @@ def setup_iverilog(vvp_name, *src_files):
            vvp_file_path, "-fst"]
     pop = subprocess.Popen(
         cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
     )
     print(f"Launched Icarus with PID {pop.pid}")
 
@@ -89,7 +90,7 @@ def send_spi(vs, tx_buffer):
         * rx_buffer: Content of the SPI slave RX buffer that was received
                      during the transaction.
     """
-    logging.info(f"Sending SPI transaction")
+    logging.info("Sending SPI transaction")
     # Set the TX buffer simulation variable with the desired content
     vs.set(path="spi_master_tb.i_spi_master.tx_buffer",
            value=tx_buffer)
@@ -109,7 +110,8 @@ def send_spi(vs, tx_buffer):
     # Let the simulator run until the named event "end_transaction" has been
     # triggered.
     vs.run(cb="until_change",
-           path="spi_master_tb.i_spi_master.end_transaction")
+           path="spi_master_tb.i_spi_master.end_transaction",
+           timeout=10.0)
 
     # Get the RX buffer simulation variable value
     answer = vs.get(sel="value",
@@ -142,8 +144,13 @@ def vs():
     pop.communicate(timeout=10)
 
 
+def get_random_tx_buffer():
+    return [random.randint(0, 255) for i in range(7)]
+
+
 def test_spi_master_simple(vs):
 
+    random.seed(57383)
     assert vs._connected
 
     # Let the simulator run for 10 us
@@ -151,21 +158,26 @@ def test_spi_master_simple(vs):
     assert answer["type"] == "ack"
 
     # Trigger an SPI transaction
-    tx = [12, 34, 56, 78, 90, 12, 34]
+    tx = get_random_tx_buffer()
+    counter_expected = 0
     rx, counter = send_spi(vs, tx)
-    assert counter == 0
+    assert counter == counter_expected
 
-    vs.run(cb="for_time", time=10, time_unit="us")
-    tx_previous = tx
-    tx = [1, 3, 5, 7, 11, 13, 17]
-    rx, counter = send_spi(vs, tx)
-    assert rx[1:] == tx_previous
-    assert counter == 1
+    for i in range(200):
+        vs.run(cb="for_time", time=10, time_unit="us")
+        tx_expected = tx
+        counter_expected += 1
+        tx = get_random_tx_buffer()
+        rx, counter = send_spi(vs, tx)
+        assert rx[1:] == tx_expected
+        assert counter == counter_expected
 
-    vs.run(cb="for_time", time=10, time_unit="us")
-    tx_previous = tx
-    tx = [19, 23, 29, 31, 37, 41, 43]
-    rx, counter = send_spi(vs, tx)
-    assert rx[1:] == tx_previous
-    assert counter == 2
 
+if __name__ == "__main__":
+
+    str_port = input("Port number: ")
+
+    with Verisocks(HOST, int(str_port)) as vs_cli:
+        vs_cli.connect()
+        test_spi_master_simple(vs_cli)
+        vs_cli.finish()
