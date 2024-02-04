794eda0dee05b2fdc8df3a065a2e2f6bc2da4bd4
jchabloz <jeremie.chabloz@a3.epfl.ch>
2024-02-04 13:31:14 +0100

    Added Hello World example

---
+++ examples/hello_world/hello_world_tb.v
@@ -0,0 +1,35 @@
+/******************************************************************************
+File: hello_world_tb.v
+Description: Hello world example testbench for Verisocks
+******************************************************************************/
+
+/*******************************************************************************
+Includes and misc definitions
+*******************************************************************************/
+`timescale 1us/10ps //Time scale definitions
+
+`ifndef VS_NUM_PORT
+`define VS_NUM_PORT 5100
+`endif
+
+`ifndef VS_TIMEOUT
+`define VS_TIMEOUT 120
+`endif
+
+/*******************************************************************************
+Testbench
+*******************************************************************************/
+module hello_world_tb();
+
+    initial begin
+
+        /* Launch Verisocks server after other initialization */
+        $verisocks_init(`VS_NUM_PORT, `VS_TIMEOUT);
+
+        /* Make sure that the simulation finishes after a while... */
+        #1000
+        $finish(0);
+
+    end
+
+endmodule
---
+++ examples/hello_world/test_hello_world.py
@@ -0,0 +1,60 @@
+from verisocks.verisocks import Verisocks
+from verisocks.utils import setup_sim, find_free_port
+import socket
+import time
+import pytest
+import logging
+import os.path
+
+
+HOST = socket.gethostbyname("localhost")
+PORT = find_free_port()
+VS_TIMEOUT = 10
+LIBVPI = "../../build/verisocks.vpi"
+CONNECT_DELAY = 0.1
+
+
+def setup_test():
+    setup_sim(
+        LIBVPI,
+        "hello_world_tb.v",
+        cwd=os.path.dirname(__file__),
+        ivl_args=[
+            f"-DVS_NUM_PORT={PORT}",
+            f"-DVS_TIMEOUT={VS_TIMEOUT}"
+        ]
+    )
+    time.sleep(CONNECT_DELAY)
+
+
+@pytest.fixture
+def vs():
+    setup_test()
+    _vs = Verisocks(HOST, PORT)
+    _vs.connect()
+    yield _vs
+    # Teardown
+    try:
+        _vs.finish()
+    except ConnectionError:
+        logging.warning("Connection error - Finish command not possible")
+    _vs.close()
+
+
+def test_hello_world(vs):
+
+    assert vs._connected
+    answer = vs.send_cmd("get", sel="sim_info")
+    assert answer['type'] == "result"
+    print(f"Simulator: {answer['product']}")
+    print(f"Version: {answer['version']}")
+
+    answer = vs.send_cmd("info", value="Hello World!")
+    assert answer['type'] == "ack"
+
+
+if __name__ == "__main__":
+
+    setup_test()
+    with Verisocks(HOST, PORT) as vs_cli:
+        test_hello_world(vs_cli)
