914e3097970afa07c564fa38af5a49970aaed4dc
jchabloz <jeremie.chabloz@a3.epfl.ch>
2025-02-20 20:34:03 +0100

    Refactor SPI master test main: improve code readability by adding comments and organizing variable registration

--- examples/spi_master/verilator/test_main.cpp
+++ examples/spi_master/verilator/test_main.cpp
@@ -11,6 +11,7 @@
 //======================
 
 int main(int argc, char** argv, char**) {
+
     // Setup context, defaults, and parse command line
     Verilated::debug(0);
     const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
@@ -19,22 +20,33 @@ int main(int argc, char** argv, char**) {
     // Construct the Verilated model, from Vtop.h generated from Verilating
     const std::unique_ptr<Vspi_master_tb> topp {new Vspi_master_tb{contextp.get()}};
 
+    // Dump public variables
     contextp->internalsDump();
 
-    /* Create top VSL instance */
+    // Create top VSL instance
     vsl::VslInteg<Vspi_master_tb> vslx{topp.get(), 5100, 5};
 
-    /* Register public variables */
-    vslx.register_scalar("spi_master_tb.miso", &topp->spi_master_tb->miso, VLVT_UINT8, 1u);
-    vslx.register_scalar("spi_master_tb.toto", &topp->spi_master_tb->toto, VLVT_REAL, 0u);
-    vslx.register_scalar("spi_master_tb.tutu", &topp->spi_master_tb->tutu, VLVT_UINT32, 32u);
-    vslx.register_array("spi_master_tb.tata", topp->spi_master_tb->tata.m_storage, VLVT_UINT8, 7u, 12u);
-    vslx.register_array("spi_master_tb.i_spi_master.rx_buffer", topp->spi_master_tb->i_spi_master->rx_buffer.m_storage, VLVT_UINT8, 8u, 8u);
-    vslx.register_array("spi_master_tb.i_spi_master.tx_buffer", topp->spi_master_tb->i_spi_master->tx_buffer.m_storage, VLVT_UINT8, 8u, 7u);
-    vslx.register_event("spi_master_tb.i_spi_master.start_transaction", &topp->spi_master_tb->i_spi_master->start_transaction);
-    vslx.register_event("spi_master_tb.i_spi_master.end_transaction", &topp->spi_master_tb->i_spi_master->end_transaction);
+    // Register public variables
+    vslx.register_scalar("spi_master_tb.miso",
+        &topp->spi_master_tb->miso, VLVT_UINT8, 1u);
+    vslx.register_scalar("spi_master_tb.toto",
+        &topp->spi_master_tb->toto, VLVT_REAL, 0u);
+    vslx.register_scalar("spi_master_tb.tutu",
+        &topp->spi_master_tb->tutu, VLVT_UINT32, 32u);
+    vslx.register_array("spi_master_tb.tata",
+        topp->spi_master_tb->tata.m_storage, VLVT_UINT8, 7u, 12u);
+    vslx.register_array("spi_master_tb.i_spi_master.rx_buffer",
+        topp->spi_master_tb->i_spi_master->rx_buffer.m_storage,
+        VLVT_UINT8, 8u, 8u);
+    vslx.register_array("spi_master_tb.i_spi_master.tx_buffer",
+        topp->spi_master_tb->i_spi_master->tx_buffer.m_storage,
+        VLVT_UINT8, 8u, 7u);
+    vslx.register_event("spi_master_tb.i_spi_master.start_transaction",
+        &topp->spi_master_tb->i_spi_master->start_transaction);
+    vslx.register_event("spi_master_tb.i_spi_master.end_transaction",
+        &topp->spi_master_tb->i_spi_master->end_transaction);
 
-    /* Run simulation */
+    // Run simulation
     vslx.run();
 
     /*
