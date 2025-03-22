d589f243fc11184506c844a4951b10d970f3082f
jchabloz <jeremie.chabloz@a3.epfl.ch>
2025-03-22 20:19:56 +0100

    Updated example using vsl-wizard

--- examples/spi_master/verilator/test_main.cpp
+++ examples/spi_master/verilator/test_main.cpp
@@ -1,12 +1,14 @@
 /*
+Note: this file has been generated from the template templates/test_main.cpp.mako
+
 Copyright (c) 2025 Jérémie Chabloz
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
@@ -28,17 +30,19 @@ SOFTWARE.
 #include <cstdlib>
 #include <memory>
 
+//======================
+
 int main(int argc, char** argv, char**) {
 
-	//Get arguments for port number and timeout
-	int port_number {5100};
-	int timeout {5};
-	if (argc > 1) {
-		port_number = std::atoi(argv[1]);
-	}
-	if (argc > 2) {
-		timeout = std::atoi(argv[2]);
-	}
+    //Get arguments for port number and timeout
+    int port_number {5100};
+    int timeout {5};
+    if (argc > 1) {
+        port_number = std::atoi(argv[1]);
+    }
+    if (argc > 2) {
+        timeout = std::atoi(argv[2]);
+    }
 
     // Setup context, defaults, and parse command line
     Verilated::debug(0);
@@ -60,14 +64,18 @@ int main(int argc, char** argv, char**) {
     vsl::VslInteg<Vspi_master_tb> vslx{topp.get(), port_number, timeout};
 
     // Register public variables
+    // Scalar variables
     vslx.register_scalar("spi_master_tb.i_spi_master.transaction_counter",
-        &topp->spi_master_tb->i_spi_master->transaction_counter, VLVT_UINT32, 32u);
+        &topp->spi_master_tb->i_spi_master->transaction_counter,
+        VLVT_UINT32, 32u);
+    // Array variables
     vslx.register_array("spi_master_tb.i_spi_master.rx_buffer",
         topp->spi_master_tb->i_spi_master->rx_buffer.m_storage,
         VLVT_UINT8, 8u, 8u);
     vslx.register_array("spi_master_tb.i_spi_master.tx_buffer",
         topp->spi_master_tb->i_spi_master->tx_buffer.m_storage,
         VLVT_UINT8, 8u, 7u);
+    // Named events
     vslx.register_event("spi_master_tb.i_spi_master.start_transaction",
         &topp->spi_master_tb->i_spi_master->start_transaction);
     vslx.register_event("spi_master_tb.i_spi_master.end_transaction",
