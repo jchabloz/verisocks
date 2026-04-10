f59955eae65629adf8178243d8267d43bd426a83
jchabloz <jeremie.chabloz@a3.epfl.ch>
2026-04-10 17:38:19 +0200

    Added logging to vsl-wizard

--- python/verisocks/cli/wizard.py
+++ python/verisocks/cli/wizard.py
@@ -23,6 +23,7 @@
 import argparse
 import pathlib
 import yaml
+import logging
 from os.path import isabs, abspath, dirname, join, relpath
 from mako.template import Template
 
@@ -79,7 +80,25 @@ option is being used)")
     parser.add_argument('--vlt-only', action='store_true',
                         help="Render variables file only (unless any other \
 *-only option is being used)")
+    parser.add_argument(
+        '--log-level', type=str,
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        default="INFO",
+        help="Logging level (default: INFO)"
+    )
+
+    # Parse arguments
     args = parser.parse_args()
+
+    # Configure logging
+    if args.log_level:
+        numeric_level = getattr(logging, args.log_level.upper(), None)
+        if not isinstance(numeric_level, int):
+            raise ValueError(f'Invalid log level: {args.log_level}')
+        logging.basicConfig(
+            format='[%(levelname)s][%(module)s] %(message)s',
+            level=numeric_level
+        )
 
     render_makefile = args.makefile_only or (
         not args.tb_only and not args.vlt_only)
@@ -94,6 +113,7 @@ option is being used)")
     template_vlt = join(args.templates_dir, "variables.vlt.mako")
 
     # Load JSON configuration file
+    logging.info(f"Loading configuration file {args.config}")
     with open(args.config, 'r') as f:
         cfg = yaml.safe_load(f)
 
@@ -102,6 +122,7 @@ option is being used)")
         if isabs(x):
             return x
         return abspath(join(dirname(args.config), x))
+
     for k in ["verisocks_root", "verilator_root", "verilator_path"]:
         format_path(cfg['config'][k])
 
@@ -128,6 +149,7 @@ option is being used)")
                         tb_file=str(args.testbench_file),
                         vlt_file=vlt_file,
                         **cfg['config'])
+        logging.info(f"Rendered {args.makefile}")
     if render_tb:
         if 'variables' in cfg:
             render_template(template_cpp, args.testbench_file,
@@ -135,9 +157,11 @@ option is being used)")
         else:
             render_template(template_cpp, args.testbench_file,
                             **cfg['config'], variables=None)
+        logging.info(f"Rendered {args.testbench_file}")
     if render_vlt and ('variables' in cfg):
         render_template(template_vlt, args.variables_file,
                         variables=cfg['variables'])
+        logging.info(f"Rendered {args.variables_file}")
 
 
 if __name__ == "__main__":
